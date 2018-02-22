94fc11786c166ad9a5527e54b9a08a6bef667dc2
jmayorga <jmayorgadev@gmail.com>
2018-02-22 08:57:18 -0500

    App component converted to a class component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import PropTypes from 'prop-types';
 import { Switch, Route } from "react-router-dom";
 import { Grid } from "semantic-ui-react";
@@ -7,33 +7,41 @@ import SignupPage from "./components/pages/SignupPage"
 import DashboardPage from "./components/pages/DashboardPage" 
 
 
-const App = ({ location }) => (
-    <Grid style={{ marginLeft: "0rem", marginRight: "0rem" }}>
-      <Grid.Row>
-        <Grid.Column mobile={16} tablet={16} computer={16}>
-          <Switch>
-            <Route
-              location={location}
-              path="/signup"
-              exact
-              component={SignupPage}
-            />
-            <Route
-              location={location}
-              path="/"
-              exact
-              component={DashboardPage}
-            />
-            <Route 
-              path="/login"
-              location={location} 
-              component={LoginPage} 
-            />
-          </Switch>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-);
+class App extends React.Component{
+  render () {
+    const {location} = this.props;
+    console.log(location);
+    return (
+      <Grid style={{ marginLeft: "0rem", marginRight: "0rem" }}>
+        <Grid.Row>
+          <Grid.Column mobile={16} tablet={16} computer={16}>
+            <Switch>
+              <Route
+                location={location}
+                path="/signup"
+                exact
+                component={SignupPage}
+              />
+              <Route
+                location={location}
+                path="/"
+                exact
+                component={DashboardPage}
+              />
+              <Route 
+                path="/login"
+                exact
+                location={location} 
+                component={LoginPage} 
+              />
+            </Switch>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    )
+  }
+}
+
 
 App.propTypes = {
 	location: PropTypes.shape({
